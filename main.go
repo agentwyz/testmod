68574de616b64785e5d1fea9559e249f089dd4e9
agentwyz <2012663060@qq.com>
2023-03-30 16:58:11 +0800

    modify main.go, add the test function

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/agentwyz/testmod/lib"
+import (
+	"github.com/agentwyz/testmod/lib"
+	"fmt"
+)
+
+
 
 func main() {
 	//直接使用go mod tidy进行导入
 	lib.Hello("wyz")
+	test()
+}
+
+//测试分支
+func test() {
+	fmt.Println("test this brach")
 }
